7296f7d6cde462f70fd82c97c7b73caef7e78796
MrVampy <than07ac@gmail.com>
2013-07-30 09:49:01 +0000

    newest commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,11 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:sort_by]
+      @movies = Movie.order(params[:sort_by])
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
@@ -37,5 +41,4 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
 end
